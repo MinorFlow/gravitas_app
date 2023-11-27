0e96cb02a02df6b3ac0ecd4e5ba596f94a43d77a
sanghyun328 <sanghyun328@naver.com>
2023-11-27 23:03:38 +0900

    calendar_change

--- lib/calendar/pages/calendar_page.dart
+++ lib/calendar/pages/calendar_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class CalendarPage extends StatefulWidget {
   const CalendarPage({super.key});
@@ -7,19 +8,25 @@ class CalendarPage extends StatefulWidget {
   State<CalendarPage> createState() => _CalendarPageState();
 }
 
-class _CalendarPageState extends State<CalendarPage> with AutomaticKeepAliveClientMixin {
-
+class _CalendarPageState extends State<CalendarPage>
+    with AutomaticKeepAliveClientMixin {
+  DateTime today = DateTime.now();
   @override
   bool get wantKeepAlive => true;
 
   @override
   Widget build(BuildContext context) {
     super.build(context);
-    return const Column(
+    return Column(
       children: [
-        Text('Calendar - ???')
+        Container(
+          child: TableCalendar(
+            focusedDay: today,
+            firstDay: DateTime.utc(2023, 10, 27),
+            lastDay: DateTime.utc(2030, 11, 27),
+          ),
+        ),
       ],
     );
   }
-
 }
