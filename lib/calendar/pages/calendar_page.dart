d8cf5b4fb8f9e75362c3965a220d4b74da9df1f0
sanghyun328 <sanghyun328@naver.com>
2023-11-28 21:20:41 +0900

    calendar_Design_1st

--- lib/calendar/pages/calendar_page.dart
+++ lib/calendar/pages/calendar_page.dart
@@ -17,15 +17,102 @@ class _CalendarPageState extends State<CalendarPage>
   @override
   Widget build(BuildContext context) {
     super.build(context);
+
+    List<bool> _selections1 = List.generate(3, (index) => false);
+
     return Column(
       children: [
         Container(
           child: TableCalendar(
+            locale: 'ko-KR',
             focusedDay: today,
-            firstDay: DateTime.utc(2001, 10, 27),
-            lastDay: DateTime.utc(2030, 11, 27),
+            firstDay: DateTime.utc(2021, 01, 01),
+            lastDay: DateTime.utc(2030, 12, 31),
+            daysOfWeekHeight: 30, // 날짜 설정
+            headerStyle: HeaderStyle(
+              formatButtonVisible: false,
+              titleCentered: true,
+              leftChevronVisible: true,
+              rightChevronVisible: true,
+              titleTextStyle: const TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.w600,
+                color: Colors.green,
+              ),
+            ), //헤더 디자인
+            calendarStyle: CalendarStyle(
+              // 켈린더 내부 디자인
+              outsideDaysVisible: false, //이전, 이후 달 보이지 않기
+              defaultTextStyle: TextStyle(
+                  //평일 날짜 디자인
+                  color: Colors.black,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500),
+              weekendTextStyle: TextStyle(
+                  //주말 날짜 디자인
+                  color: Colors.red,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500),
+              todayDecoration: BoxDecoration(
+                  // 오늘 날짜 디자인
+                  color: Colors.transparent,
+                  shape: BoxShape.circle,
+                  border: Border.all(color: Colors.blue, width: 3)),
+              todayTextStyle: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w900,
+                  color: Colors.blue),
+              tableBorder: const TableBorder(
+                top: BorderSide(color: Colors.black),
+                horizontalInside: BorderSide(color: Colors.black),
+                verticalInside: BorderSide(color: Colors.black),
+              ),
+              rowDecoration: const BoxDecoration(
+                color: Colors.white30,
+              ),
+              selectedDecoration: const BoxDecoration(
+                color: Colors.brown,
+              ),
+            ),
           ),
-        ),
+        ), // 캘린더
+        Container(
+          child: ToggleButtons(
+            renderBorder: true,
+            borderRadius: BorderRadius.circular(10),
+            borderWidth: 10,
+            borderColor: Colors.white,
+            selectedColor: Colors.blue,
+            fillColor: Colors.blue,
+            color: Colors.black12,
+            children: [
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 50),
+                child: Text(
+                  "Today",
+                  style: TextStyle(fontSize: 25),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 40),
+                child: Text(
+                  "ALL",
+                  style: TextStyle(fontSize: 25),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: Icon(Icons.tune),
+              ),
+            ],
+            onPressed: (int index) {
+              setState(() {
+                _selections1[index] = !_selections1[index];
+              });
+            },
+            isSelected: _selections1,
+          ),
+        ), // 토글 버튼
       ],
     );
   }
