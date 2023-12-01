38dd89f881af82a3ed82460b6cc6e4c96294d686
Seunghun Lee <ljykdltmdgns624@gmail.com>
2023-12-01 13:36:52 +0900

    Revert "[Feat] C. 활동 페이지 UI 구현 (#10)"
    This reverts commit 333cd8fb211ed312f29711fd7c91d7b5c2eef102.

--- lib/activity/pages/activity_page.dart
+++ lib/activity/pages/activity_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:gravitas_app/activity/widgets/activity_tuple.dart';
-import 'package:gravitas_app/activity/widgets/activity_dropdown.dart';
 
 class ActivityPage extends StatefulWidget {
   const ActivityPage({super.key});
@@ -9,52 +7,19 @@ class ActivityPage extends StatefulWidget {
   State<ActivityPage> createState() => _ActivityPageState();
 }
 
-class _ActivityPageState extends State<ActivityPage>
-    with AutomaticKeepAliveClientMixin {
+class _ActivityPageState extends State<ActivityPage> with AutomaticKeepAliveClientMixin {
+
   @override
   bool get wantKeepAlive => true;
 
   @override
   Widget build(BuildContext context) {
     super.build(context);
-    return Column(
+    return const Column(
       children: [
-        Container(
-          height: 48,
-          color: Colors.black,
-          child: Row(
-            children: <Widget>[
-              const SizedBox(
-                width: 12,
-              ),
-              const Widget090(),
-              const Expanded(
-                child: SizedBox(width: double.maxFinite),
-              ),
-              IconButton(
-                icon: const Icon(Icons.tune),
-                color: Colors.white,
-                iconSize: 24,
-                onPressed: () {
-                  setState(() {});
-                },
-              ),
-            ],
-          ),
-        ),
-        const Column(
-          children: <Widget>[
-            ActivityTuple(),
-            ActivityTuple(),
-            ActivityTuple(),
-            ActivityTuple(),
-            ActivityTuple(),
-            ActivityTuple(),
-            ActivityTuple(),
-            ActivityTuple(),
-          ],
-        ),
+        Text('Activity')
       ],
     );
   }
+
 }
